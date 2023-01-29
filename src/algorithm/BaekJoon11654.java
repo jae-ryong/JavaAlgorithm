a3e579cedfd4f8669c5f5c1da581cd802eff0b56
JaeRyong <frogs6225@naver.com>
2023-01-29 20:08:55 +0900

    second problem

---
+++ src/algorithm/BaekJoon11654.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+import java.util.Scanner;
+
+// น้มุ 11654น๘
+public class BaekJoon11654 {
+
+	public static void main(String[] args) {
+		 Scanner sc =new Scanner(System.in);
+		 String a = sc.next();
+		 int ch = a.charAt(0);
+		 System.out.println(ch);
+	}
+}
